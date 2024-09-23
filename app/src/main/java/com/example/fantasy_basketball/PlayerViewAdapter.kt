cc9f91150e0a079413508215afd3f1e4d6e1a8b2
Juilliard <Juilliardwu0405@gmail.com>
2024-09-23 11:34:17 -0400

    fix:Remove unused import

--- app/src/main/java/com/example/fantasy_basketball/PlayerViewAdapter.kt
+++ app/src/main/java/com/example/fantasy_basketball/PlayerViewAdapter.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import com.example.fantasy_basketball.databinding.FragmentPlayerSearchBinding
-
 /**
  * [RecyclerView.Adapter] that can display a [players].
  *
